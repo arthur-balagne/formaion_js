52d94b42fa437f27164a701f467ed38d54350c31
arthur-balagne <arthur97115@gmail.com>
2015-10-06 10:39:40 +0200

    1 updates

--- 1/app.js
+++ 1/app.js
@@ -1,1 +1,9 @@
-console.log('javascript from app ready to serve !!');
+
+var variable_1 = 'Hello world';
+var variable_2 =  1;
+var variable_3 =  {
+	1 : 'hello',
+	2 : 'world',
+	3 : 2
+};
+var variable_1 = 'Hello world';
